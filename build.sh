8f6504735ffdf2e4d2e34f51639d6bba14eaa9a4
paul22440 <cjp2crt@bolton.ac.uk>
2026-09-07 22:42:37 +0100

    Fix Render admin login

--- build.sh
+++ build.sh
@@ -10,16 +10,24 @@ python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 import os
+
 User = get_user_model()
 username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
 password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
 
-if username and password and not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(
-        username=username,
-        password=password
-    )
-    print('Superuser created successfully.')
+if username and password:
+    user, created = User.objects.get_or_create(username=username)
+
+    user.set_password(password)
+    user.is_staff = True
+    user.is_superuser = True
+    user.is_active = True
+    user.save()
+
+    if created:
+        print('Superuser created successfully.')
+    else:
+        print('Existing user updated to superuser successfully.')
 else:
-    print('Superuser already exists or credentials are missing.')
+    print('Superuser credentials are missing.')
 "
