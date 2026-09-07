4518105995da74caf932bddc480a1666ce0348b4
paul22440 <cjp2crt@bolton.ac.uk>
2026-09-07 22:27:07 +0100

    Create Render admin use

--- build.sh
+++ build.sh
@@ -6,3 +6,20 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        password=password
+    )
+    print('Superuser created successfully.')
+else:
+    print('Superuser already exists or credentials are missing.')
+"
